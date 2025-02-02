b505b06a62a648a95fb05fe86c439214b33399cd
Bruno R. Pianca <brunorpdev@gmail.com>
2025-02-02 01:24:03 -0300

    feat: Ciração model de orçamentos

---
+++ apps/orcamentos/models.py
@@ -0,0 +1,25 @@
+from apps.clientes.models import Cliente
+from django.contrib.auth.models import User
+from django.db import models
+from shared.models import BaseModel
+
+
+class Orcamento(BaseModel):
+    class StatusChoices(models.TextChoices):
+        EM_DESENVOLVIMENTO = 'em_desenvolvimento', 'Em desenvolvimento'
+        AGUARDANDO_CLIENTE = 'aguardando_cliente', 'Aguardando cliente'
+        APROVADO = 'aprovado', 'Aprovado'
+        RECUSADO = 'recusado', 'Recusado'
+
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    ambiente = models.CharField('Ambiente', max_length=255)
+    status = models.CharField(max_length=20, choices=StatusChoices.choices, default=StatusChoices.EM_DESENVOLVIMENTO)
+    valor_total = models.DecimalField('Valor do projeto', max_digits=10, decimal_places=2, default=0.00)
+
+    class Meta:
+        verbose_name = 'Orçamento'
+        verbose_name_plural = 'Orçamentos'
+    
+    def __str__(self):
+        return f'Orçamento {self.ambiente} = {self.cliente}'
--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.clientes',
     'apps.empresas',
+    'apps.orcamentos',
 ]
 
 MIDDLEWARE = [
