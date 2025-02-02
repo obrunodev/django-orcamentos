1d84d8035d80c6b4db2b70a9385e0c22a515d6b9
Bruno R. Pianca <brunorpdev@gmail.com>
2025-02-01 22:46:17 -0300

    refac: Chamando o isinstance apenas uma vez

--- shared/forms.py
+++ shared/forms.py
@@ -19,12 +19,12 @@ class BaseModelForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            if any([
-                isinstance(field.widget, forms.TextInput),
-                isinstance(field.widget, forms.Textarea),
-                isinstance(field.widget, forms.NumberInput),
-                isinstance(field.widget, forms.EmailInput),
-            ]):
+            if isinstance(field.widget, (
+                forms.TextInput, 
+                forms.Textarea,
+                forms.NumberInput,
+                forms.EmailInput
+            )):
                 field.widget.attrs.setdefault('class', 'form-control')
             elif isinstance(field.widget, forms.Select):
                 field.widget.attrs.setdefault('class', 'form-select')
